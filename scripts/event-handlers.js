974cee5217d8f92d1b018a2b633dcfff76bc8ad1
tori4582 <hungtran6360@gmail.com>
2021-11-06 11:46:41 +0700

    feat: active menu item when scrolling

--- scripts/event-handlers.js
+++ scripts/event-handlers.js
@@ -34,11 +34,59 @@ function changeBackgroundWhenScrolling() {
     }
 }
 
+function getSectionPositions() {
+    let sections = document.getElementsByTagName("section");
+    let sectionPositions = [];
+    for (let s of sections) {
+        sectionPositions.push(s.offsetTop);
+    }
+    return sectionPositions;
+}
+
+function getCurrentIndex() {
+    let currentIndex = -1;
+    let y = window.scrollY;
+    let triggerPoint = document.documentElement.clientHeight / 2;
+    let menuBarHeight = document.getElementById('menubar').offsetHeight;
+    let sectionPositions = getSectionPositions();
+
+    for (let i = 0; i < sectionPositions.length; i++) {
+        if (y + triggerPoint >= sectionPositions[i]) {
+            currentIndex = i;
+            // console.log(y + " " + sectionPositions[i]);
+        }
+    }
+
+    return currentIndex;
+}
+
+function changeActiveLinkWhenScrolling() {
+    let y = window.scrollY;
+    let currentIndex = getCurrentIndex();
+
+    let menuItems = document.getElementsByClassName("indexes__item");
+    for (let p of menuItems) {
+        p.classList.remove("active");
+    }
+
+    if (currentIndex !== -1) {
+        menuItems[currentIndex].classList.add("active");
+    }
+
+}
+
+let sectionPositions = [];
+let menuItems = [];
+
 // Main method: fetch HTML Elements only work when the page is fully loaded.
 window.onload = function() {
     addHoverEventForCardHeaders();
+
+    sectionPositions = getSectionPositions();
+
 };
 
 window.onscroll = function() {
     changeBackgroundWhenScrolling();
+    changeActiveLinkWhenScrolling();
 }
