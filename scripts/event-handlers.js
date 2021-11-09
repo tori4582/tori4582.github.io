864fea372f1bf81d326f61866d98132968c0de0b
tori4582 <hungtran6360@gmail.com>
2021-11-09 22:43:56 +0700

    feat: 'Experiences' section and complete the version

--- scripts/event-handlers.js
+++ scripts/event-handlers.js
@@ -1,25 +1,3 @@
-let extractCardHeader = function(element) {
-    return element.getElementsByTagName("h1")[0];
-}
-
-let cardEvents = {
-    hover: function(event) {
-        extractCardHeader(event.srcElement).classList.add("hovered");
-    },
-    exit: function(event) {
-        extractCardHeader(event.srcElement).classList.remove("hovered");
-    }
-};
-
-let addHoverEventForCardHeaders = function() {
-    let cards = document.getElementsByClassName("card");
-
-    for (let card of cards) {
-        card.addEventListener("mouseover", cardEvents.hover, true);
-        card.addEventListener("mouseout", cardEvents.exit, true);
-    }
-}
-
 function changeBackgroundWhenScrolling() {
     let vh = document.documentElement.clientHeight;
     let y = window.scrollY;
@@ -75,14 +53,8 @@ function changeActiveLinkWhenScrolling() {
 
 }
 
-let sectionPositions = [];
-let menuItems = [];
-
 // Main method: fetch HTML Elements only work when the page is fully loaded.
 window.onload = function() {
-    addHoverEventForCardHeaders();
-
-    sectionPositions = getSectionPositions();
 
 };
 
